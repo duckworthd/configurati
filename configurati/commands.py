f5056680350615dd18c30c6e292fbdd1307c3c0b
Daniel Duckworth <duckworthd@gmail.com>
2013-10-08 00:39:44 -0700

    remove configure from commands.py

--- configurati/commands.py
+++ configurati/commands.py
@@ -72,30 +72,3 @@ def env(variable_name):
   """
   return os.environ[variable_name]
 
-
-def configure(args=None, config=None, spec=None):
-  # load command line arguments
-  if args is None:
-    args = sys.argv[1:]
-  if args is not None:
-    args = load(args)
-  else:
-    args = {}
-
-  # load configuration file
-  if config is None and 'config' in args:
-    config = args['config']
-  if config is not None:
-    result = update(args, load_config(config))
-  else:
-    result = args
-
-  # apply config spec
-  if spec is None and 'spec' in args:
-    spec = args['spec']
-  if spec is not None:
-    result = validate(load_spec(spec), result)
-
-  # set globally and return
-  CONFIG(result)
-  return CONFIG()
