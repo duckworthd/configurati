756403bc99c4b50e5660af6182e6db5b858fa715
Daniel Duckworth <duckworthd@gmail.com>
2013-10-09 07:19:19 -0700

    added list construction, key validation to attrs, no build option in get()

--- configurati/attrs.py
+++ configurati/attrs.py
@@ -7,7 +7,7 @@ from .exceptions import ConfiguratiException
 from .utils import recursive_apply, Missing
 
 
-class attrs(dict):
+class adict(dict):
   """A dict you can access with dot notation"""
 
   def __getattr__(self, key):
@@ -20,6 +20,30 @@ class attrs(dict):
     self[key] = value
     return self[key]
 
+
+class attrs(adict):
+  """A dictionary/object designed for nested objects"""
+
+  def __init__(self, *args, **kwargs):
+    super(attrs, self).__init__()
+
+    # get key, value pairs passed in
+    if len(args) == 0:
+      a = []
+    elif len(args) == 1:
+      a = args[0]
+      if isinstance(a, dict):
+        a = a.items()
+    elif len(args) > 1:
+      raise TypeError(
+          "dict expected at most 1 arguments, got {}".format(len(args))
+        )
+    a += kwargs.items()
+
+    # add them all, one by one
+    for k, v in a:
+      self[k] = v
+
   def __getitem__(self, key):
     if is_atomic(key):
       return super(attrs, self).__getitem__(key)
@@ -27,6 +51,10 @@ class attrs(dict):
       return get(self, '.' + key)
 
   def __setitem__(self, key, value):
+    if not valid_key(key):
+      raise KeyError(
+          "invalid attrs key: {}".format(key)
+        )
     value = attrs.from_dict(value)
     if is_atomic(key):
       return super(attrs, self).__setitem__(key, value)
@@ -56,22 +84,20 @@ def is_atomic(key):
   return not ('[' in key or '.' in key)
 
 
-def get(obj, key, build=False):
+def get(obj, key):
   """Retrieve a key from a nested object"""
   if len(key) == 0:
     return obj
   else:
     key, rest = next_key(key)
     if isinstance(obj, dict):
-      if build and key not in obj:
-        obj[key] = {}
-      return get(obj[key], rest, build)
+      return get(obj[key], rest)
     elif isinstance(obj, list) or isinstance(obj, tuple):
       if not isinstance(key, int):
         raise KeyError('Attempting to use non-integer index "{}" on list or tuple'.format(key))
       if key >= len(obj):
         raise KeyError('Attempting to access index {} but only {} available'.format(key, len(obj)))
-      return get(obj[key], rest, build)
+      return get(obj[key], rest)
     else:
       raise KeyError('Still have additional key components "{}" but have already reached terminal node {}'.format(key, obj))
 
@@ -81,12 +107,8 @@ def set(obj, key, value, build=False):
   if len(key) == 0:
     return value
   else:
-    key, rest = next_key(key)
-
     def set_dict(obj, key, value):
-      if build and key not in obj:
-        obj[key] = {}
-      obj[key] = set(obj[key], rest, value, build)
+      obj[key] = set(obj.get(key, Missing), rest, value, build)
       return obj
 
     def set_list(obj, key, value):
@@ -97,13 +119,27 @@ def set(obj, key, value, build=False):
       obj[key] = set(obj[key], rest, value, build)
       return obj
 
+    def set_tuple(obj, key, value):
+      return tuple(set_list(list(obj), key, value))
+
+    # parse next atomic key
+    key, rest = next_key(key)
+
+    # make an object if `build` and object is missing
+    if build and obj is Missing:
+      if isinstance(key, basestring):
+        obj = {}
+      elif isinstance(key, int):
+        obj = [Missing] * (key + 1)
+      else:
+        raise KeyError("Unrecognized key type: {}".format(str(type(key))))
 
     if isinstance(obj, dict):
       return set_dict(obj, key, value)
     elif isinstance(obj, list):
      return set_list(obj, key, value)
     elif isinstance(obj, tuple):
-      return tuple(set_list(list(obj), key, value))
+      return set_tuple(obj, key, value)
     else:
       raise KeyError('Still have additional key components "{}" but have already reached terminal node {}'.format(key, obj))
 
@@ -111,26 +147,32 @@ def set(obj, key, value, build=False):
 def next_key(s):
   if s.startswith('.'):
     # get "key_1" from ".key_1[0]" or ".key_1.key_2"
-    match = re.search("""[.]([^.[]+)""", s)
+    match = re.search("""^[.]([^.[]*)""", s)
+    key, rest = match.group(1), s[match.end():]
+    if re.search("""^[a-zA-Z][a-zA-Z0-9-_]*$""", key) is None:
+      raise KeyError('"{}" is not a attrs key'.format(key))
+    return key.replace("-", "_"), rest
   elif s.startswith('['):
     # get "5" from "[5].key_1" or "[5][3]"
-    match = re.search("""\[([^[.]+)\]""", s)
-  else:
-    match = None
-
-  if match is not None:
+    match = re.search("""^\[([^[.]*)\]""", s)
     key, rest = match.group(1), s[match.end():]
-    return parse_key(key), rest
+    try:
+      key = int(key)
+    except ValueError:
+      raise KeyError('"{}" is not a valid list/tuple key'.format(key))
+    return key, rest
   else:
-    raise ConfiguratiException("Unable to parse key: {}".format(s))
+    raise KeyError('"{}" is not a valid attrs key'.format(s))
 
 
-def parse_key(s):
-  if '-' in s:
-    s = s.replace("-", "_")
-  try:
-    s = int(s)
-  except ValueError:
-    pass
-  return s
-
+def valid_key(k):
+  if not isinstance(k, basestring):
+    return False
+  else:
+    k = '.' + k
+    while len(k) > 0:
+      try:
+        _, k = next_key(k)
+      except KeyError:
+        return False
+    return True
--- configurati/tests/test_attrs.py
+++ configurati/tests/test_attrs.py
@@ -21,11 +21,34 @@ class AttrsTests(unittest.TestCase):
         })
       })
 
+  def test_init(self):
+    a1 = attrs({'a': 1, 'b': 2})
+    a2 = attrs([('a', 1), ('b', 2)])
+    a3 = attrs(a=1, b=2)
+    a = attrs()
+    a['a'] = 1
+    a['b'] = 2
+    self.assertEqual(a1, a)
+    self.assertEqual(a2, a)
+    self.assertEqual(a3, a)
+
+  def test_invalid_init(self):
+    # keys must be strings
+    self.assertRaises(KeyError, attrs, {123: 1})
+
+    # keys cannot start with non-letters
+    self.assertRaises(KeyError, attrs, {"1a": 1})
+    self.assertRaises(KeyError, attrs, {"-a": 1})
+
   def test_from_dict(self):
+    # dicts converted to attrs, others left alone
     a = attrs.from_dict(self.o)
     self.assertIsInstance(a, attrs)
     self.assertIsInstance(a['b'], attrs)
     self.assertIsInstance(a['b']['c'], list)
+
+    # validate keys on from_dict
+    self.assertRaises(KeyError, attrs.from_dict, {"a": {123: 2}})
 
   def test_getattr(self):
     self.assertEqual(self.a.b, self.a['b'])
@@ -38,13 +61,22 @@ class AttrsTests(unittest.TestCase):
     self.assertEqual(self.a.c, 1)
 
   def test_setattr_dict(self):
-    self.a.x = {'a': 1, 'b': 2}
+    # dicts get converted to attrs
+    self.a.x = {'a': 1, 'b': { 'c': 2 }}
     self.assertEqual(self.a.x.a, 1)
+    self.assertEqual(self.a.x.b.c, 2)
+
+    # validate keys on setattr
+    def f():
+      self.a.y = {1: 2}
+    self.assertRaises(KeyError, f)
 
   def test_getitem(self):
+    # atomic keys
     self.assertEqual(self.a['b']['c'], [1,2,3])
 
   def test_getitem_nested(self):
+    # dot and bracket notation
     self.assertEqual(self.a['b.c[2]'], self.a['b']['c'][2])
 
   def test_setitem(self):
@@ -52,8 +84,13 @@ class AttrsTests(unittest.TestCase):
     self.assertEqual(self.a.b, 1)
 
   def test_setitem_nested(self):
-    self.a['x.y.z'] = 1
-    self.assertEqual(self.a['x'], attrs.from_dict({'y': {'z': 1}}))
+    # attrs, list construction
+    self.a['x.y[1].z'] = 1
+    self.assertEqual(self.a['x'], attrs.from_dict({'y': [Missing, {'z': 1}]}))
+
+    # list extension
+    self.a['b.c[5]'] = 10
+    self.assertEqual(self.a['b']['c'], [1, 2, 3, Missing, Missing, 10])
 
   def test_contains(self):
     self.assertIn('a', self.a)
@@ -61,8 +98,10 @@ class AttrsTests(unittest.TestCase):
 
   def test_contains_nested(self):
     self.assertIn('b.c[2]', self.a)
-    self.assertNotIn('b.c[3]', self.a)
-    self.assertNotIn('b.e', self.a)
+    self.assertNotIn('b.c.2', self.a)   # .2 isn't a valid identifier
+    self.assertNotIn('b.c[3]', self.a)  # list too short
+    self.assertNotIn('b.e', self.a)     # missing dict key
+
 
 class NextKeyTests(unittest.TestCase):
 
@@ -77,6 +116,12 @@ class NextKeyTests(unittest.TestCase):
 
   def test_bracket_then_bracket(self):
     assert next_key(      "[5][3]") == (         5,  "[3]")
+
+  def test_invalid_key(self):
+    self.assertRaises(KeyError, next_key, "a.b")
+
+  def test_invalid_attrs_key(self):
+    self.assertRaises(KeyError, next_key, ".1.a")
 
 
 class GetTests(unittest.TestCase):
@@ -101,9 +146,6 @@ class GetTests(unittest.TestCase):
 
   def test_empty_string(self):
     assert get(self.o, "") == self.o
-
-  def test_build(self):
-    assert get(self.o, ".a.z", build=True) == {}
 
   def test_remaining_key(self):
     self.assertRaises(KeyError, get, self.o, ".a.b.c")
@@ -145,3 +187,36 @@ class SetTests(unittest.TestCase):
   def test_short_list(self):
     set(self.o, ".a.b[5]", 1)
     assert self.o['a']['b'][5] == 1
+
+
+class ValidKeyTests(unittest.TestCase):
+
+  def setUp(self):
+    self.a = attrs({
+        'a': 'abc',
+        'b': attrs({
+          'c': [1,2,3],
+          'd': 'zyx',
+        })
+      })
+
+  def test_simple(self):
+    self.assertTrue(valid_key("a"))
+
+  def test_nested(self):
+    self.assertTrue(valid_key("a.b.c"))
+
+  def test_brackets(self):
+    self.assertTrue(valid_key("a[1].c"))
+
+  def test_leading_digit(self):
+    self.assertFalse(valid_key("1a"))
+
+  def test_leading_bracket(self):
+    self.assertFalse(valid_key("[1]"))
+
+  def test_leading_period(self):
+    self.assertFalse(valid_key(".a"))
+
+  def test_non_integer_in_bracket(self):
+    self.assertFalse(valid_key(".a[ha]"))
